e9ba413b47a2e1b29e0f5b18c7c52af727451f69
aaranja <fidxle@gmail.com>
2022-05-11 22:28:18 -0700

    Settings app reformatted. Packages are now updated.

--- backend/src/etitulacion/urls.py
+++ backend/src/etitulacion/urls.py
@@ -1,7 +1,7 @@
 """etitulacion URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,22 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path, include
+from dj_rest_auth.views import PasswordResetConfirmView
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
-    path('admin/', include('users.admin.urls')),
-    path('accounts/', include('allauth.urls')),
-    path('api-auth/', include('rest_framework.urls')),
-    path('rest-auth/', include('rest_auth.urls')),
-    # path('rest-auth/registration/', include('users.auth.register_url')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    path('graduate/', include('users.graduate.urls')),
-    path('staff/', include('users.staff.urls')),
-    path('authenticate/', include('users.auth.urls')),
-    path('api/', include('documents.urls')),
-    path('api/', include('process.urls')),
-
+    path('admin/', admin.site.urls),
+    path('auth/password/reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('auth/', include('dj_rest_auth.urls')),
+    path('auth/', include('users.urls')),
 ]
